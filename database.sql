d698c7affa943e81cd7d77252a71fd5ed456d980
CinematicGenius007 <ayush3642.be20@chitkara.edu.in>
2023-03-01 10:23:47 +0530

    Added database schema

---
+++ database.sql
@@ -0,0 +1,44 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `handle` varchar(40) NOT NULL,
+  `profile` varchar(200) DEFAULT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tweets` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tweet` text NOT NULL,
+  `likes` int DEFAULT '0',
+  `media` varchar(200) DEFAULT NULL,
+  `user` int NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_user` (`user`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `likes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tweet` int NOT NULL,
+  `user` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `un_tweet_user` (`tweet`,`user`),
+  KEY `fk_likes_user` (`user`),
+  CONSTRAINT `fk_likes_tweet` FOREIGN KEY (`tweet`) REFERENCES `tweets` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_likes_user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `replies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `reply` text NOT NULL,
+  `tweet` int NOT NULL,
+  `user` int NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_reply_user` (`user`),
+  KEY `fk_tweet` (`tweet`),
+  CONSTRAINT `fk_reply_user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_tweet` FOREIGN KEY (`tweet`) REFERENCES `tweets` (`id`) ON DELETE CASCADE
+);
